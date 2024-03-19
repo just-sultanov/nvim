6d668b0e92914fade4ff752c6b499ea635fcecda
Ilshat Sultanov <ilshat@sultanov.team>
2024-03-19 13:09:02 +0300

    Enable cljstyle

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -1,11 +1,11 @@
 return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
-    -- local null_ls = require "null-ls"
-    -- config.sources = {
-    -- null_ls.builtins.diagnostics.clj_kondo,
-    -- null_ls.builtins.formatting.cljstyle,
-    -- }
+    local null_ls = require "null-ls"
+    config.sources = {
+      null_ls.builtins.diagnostics.clj_kondo,
+      null_ls.builtins.formatting.cljstyle,
+    }
     return config
   end,
 }
