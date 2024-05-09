5a3315b20cd3fca3eca6eef4534704e80a8f4ab9
Ilshat Sultanov <ilshat@sultanov.team>
2024-05-09 13:06:17 +0300

    Disable `nix` community pack

--- lua/community.lua
+++ lua/community.lua
@@ -31,7 +31,7 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.nix" },
+  -- { import = "astrocommunity.pack.nix" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.tailwindcss" },
