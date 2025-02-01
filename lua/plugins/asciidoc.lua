62cf157d11812780f9f9a0e2d02512081900cd27
Ilshat Sultanov <ilshat@sultanov.team>
2025-02-02 00:08:16 +0300

    Add asciidoctor preview plugin

---
+++ lua/plugins/asciidoc.lua
@@ -0,0 +1,13 @@
+-- Asciidoctor
+
+---@type LazySpec
+return {
+  {
+    "tigion/nvim-asciidoc-preview",
+    ft = { "asciidoc" },
+    build = "cd server && npm install",
+    opts = {
+      -- Add user configuration here
+    },
+  },
+}
