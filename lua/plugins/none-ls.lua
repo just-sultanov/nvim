d7de3f9dfb1a9e969974badf17e2ec7c1e78aa96
Ilshat Sultanov <ilshat@sultanov.team>
2025-02-02 02:24:30 +0300

    Add sqruff and ltrs builtins

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -3,6 +3,9 @@
 ---@type LazySpec
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    { "AstroNvim/astrolsp", opts = {} },
+  },
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
     local null_ls = require "null-ls"
@@ -12,12 +15,18 @@ return {
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
       -- Set a formatter
+      null_ls.builtins.diagnostics.clj_kondo,
+      null_ls.builtins.diagnostics.ltrs,
       null_ls.builtins.formatting.stylua,
       null_ls.builtins.formatting.prettier,
-      null_ls.builtins.diagnostics.clj_kondo,
-      -- null_ls.builtins.formatting.cljstyle,
-      null_ls.builtins.formatting.pg_format,
+      null_ls.builtins.formatting.cljstyle,
+      -- null_ls.builtins.formatting.pg_format,
+      -- A high-speed SQL linter written in Rust.
+      null_ls.builtins.diagnostics.sqruff,
     }
+
+    config.on_attach = require("astrolsp").on_attach
+
     return config
   end,
 }
