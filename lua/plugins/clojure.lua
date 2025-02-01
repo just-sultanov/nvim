e8002a6d7b72620715ac2633e35c98df1cfc24fe
Ilshat Sultanov <ilshat@sultanov.team>
2025-02-02 00:09:34 +0300

    Fix deprecation warnings

--- lua/plugins/clojure.lua
+++ lua/plugins/clojure.lua
@@ -23,7 +23,7 @@ return {
       vim.api.nvim_create_autocmd("BufNewFile", {
         group = vim.api.nvim_create_augroup("conjure_log_disable_lsp", { clear = true }),
         pattern = { "conjure-log-*" },
-        callback = function() vim.diagnostic.disable(0) end,
+        callback = function() vim.diagnostic.enable(false) end,
         desc = "Conjure Log disable LSP diagnostics",
       })
 
