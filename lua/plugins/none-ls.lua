7f38f2c692108d1dfce605cc786ffc68cca6f562
Ilshat Sultanov <ilshat@sultanov.team>
2025-09-25 23:35:13 +0300

    Split formatters and linters by language and add sql formatter

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -14,16 +14,22 @@ return {
     -- Only insert new sources, do not replace the existing ones
     -- (If you wish to replace, use `opts.sources = {}` instead of the `list_insert_unique` function)
     opts.sources = require("astrocore").list_insert_unique(opts.sources, {
-      -- Set a linter
-      null_ls.builtins.diagnostics.clj_kondo,
+      -- SQL
       null_ls.builtins.diagnostics.sqruff,
-      null_ls.builtins.diagnostics.swiftlint,
+      null_ls.builtins.formatting.sqruff,
 
-      -- Set a formatter
-      null_ls.builtins.formatting.stylua,
-      null_ls.builtins.formatting.prettier,
+      -- Clojure
+      null_ls.builtins.diagnostics.clj_kondo,
       null_ls.builtins.formatting.cljstyle,
 
+      -- Lua
+      null_ls.builtins.formatting.stylua,
+
+      -- JS/Markdown/etc
+      null_ls.builtins.formatting.prettier,
+
+      -- Swift
+      null_ls.builtins.diagnostics.swiftlint,
       null_ls.builtins.formatting.swiftformat, -- https://github.com/nicklockwood/SwiftFormat
       -- null_ls.builtins.formatting.swift_format, -- https://github.com/apple/swift-format
     })
